0712ad4fa3a42873dded0b7d1c57a7051a5ecbd9
Chaser <florian.isak@icloud.com>
2024-05-27 22:37:23 +0200

    Update api

--- build/lib/e_stock/models/associations.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Table, Column, ForeignKey
-from e_stock.core.database import Base
-from sqlalchemy_utils import UUIDType
-
-product_category_association = Table(
-    'product_category', Base.metadata,
-    Column('product_id', UUIDType, ForeignKey('products.id'), primary_key=True),
-    Column('category_id', UUIDType, ForeignKey('categories.id'), primary_key=True),
-)
