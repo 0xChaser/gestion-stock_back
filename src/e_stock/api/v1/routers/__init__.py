5da1831ebde946ae6f61ee2110f310b143704e41
Chaser <florian.isak@icloud.com>
2024-06-16 17:45:21 +0200

    Update product without 's'

--- src/e_stock/api/v1/routers/__init__.py
+++ src/e_stock/api/v1/routers/__init__.py
@@ -1,12 +1,12 @@
 from e_stock.api.v1.routers.categories import router as category_router
 from e_stock.api.v1.routers.products import router as product_router
-# from e_stock.api.v1.routers.stocks import router as stock_router
+from e_stock.api.v1.routers.stocks import router as stock_router
 from fastapi import APIRouter
 
 __all__ = [
     category_router,
     product_router,
-    # stock_router
+    stock_router
 ]
 
 router = APIRouter(prefix="/v1")
--- src/e_stock/api/v1/routers/stocks.py
+++ src/e_stock/api/v1/routers/stocks.py
@@ -1,34 +1,24 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
+from e_stock.models.stocks import StockPublic, StockCreate, StockPatch
 from e_stock.core.database import get_db_session
+from sqlmodel.ext.asyncio.session import AsyncSession
 from e_stock.repositories.stocks import StockRepository
-from e_stock.schemas.stocks import StockOut, StockBase
-from typing import List
 from uuid import UUID
 
-router = APIRouter(prefix="/stock", tags=["stock"])
 
-@router.get('/', response_model=List[StockOut])
+router = APIRouter(prefix="/stock", tags=['stock'])
+
+@router.get('/', response_model=list[StockPublic])
 async def list_stocks(session: AsyncSession = Depends(get_db_session)):
     stock_repo = StockRepository(session)
     return await stock_repo.list()
 
-@router.get('/{id}', response_model=StockOut)
-async def get_stock_by_id(id: UUID, session: AsyncSession = Depends(get_db_session)):
-    stock_repo = StockRepository(session)
-    return await stock_repo.get_by_id(id)
-
-@router.post('/', response_model=StockOut)
-async def create_stock(stock: StockBase, session: AsyncSession = Depends(get_db_session)):
+@router.post('/', response_model=StockPublic)
+async def create_stock(stock: StockCreate, session: AsyncSession = Depends(get_db_session)):
     stock_repo = StockRepository(session)
     return await stock_repo.add(stock)
 
-@router.patch('/{id}', response_model=StockOut)
-async def update_stock(id: UUID, stock: StockBase, session: AsyncSession = Depends(get_db_session)):
+@router.patch('/{id}', response_model=StockPublic)
+async def patch_stock(id: UUID, stock: StockPatch, session: AsyncSession = Depends(get_db_session)):
     stock_repo = StockRepository(session)
-    return await stock_repo.update(id, stock)
-
-@router.delete('/{id}', status_code=204)
-async def delete_stock(id: UUID, session: AsyncSession = Depends(get_db_session)):
-    stock_repo = StockRepository(session)
-    return await stock_repo.delete(id)
+    return await stock_repo.patch(id, stock)
