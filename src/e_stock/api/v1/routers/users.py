f995fed87167e1366ed3609975dbfeb28b0aa7ba
Chaser <florian.isak@icloud.com>
2024-06-17 12:09:18 +0200

    Fix User endpoints

--- src/e_stock/api/v1/routers/users.py
+++ src/e_stock/api/v1/routers/users.py
@@ -7,12 +7,12 @@ from e_stock.models.users import User
 from e_stock.repositories.users import UserRepository
 from e_stock.schemas.users import UserRead, UserUpdate
 
-router = APIRouter(prefix="/user", tags=["users"])
+router = APIRouter(tags=["users"])
 
 router.include_router(fastapi_users.get_users_router(UserRead, UserUpdate), prefix="/user", tags=["users"])
 
 
-@router.get("/", response_model=list[UserRead])
+@router.get("/users", response_model=list[UserRead])
 async def list_users(session: AsyncSession = Depends(get_user_db), user: User = Depends(current_superuser)):
     user_repo = UserRepository(session)
     return await user_repo.get_all_users()
